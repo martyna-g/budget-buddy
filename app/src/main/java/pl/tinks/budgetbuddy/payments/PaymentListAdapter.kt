9a7d72868d8e353bc64dcb6a91a1415a365fcd45
Martyna Gorna <mart.gorna@gmail.com>
2023-11-27 17:27:09 +0000

    Add PaymentListAdapter with ViewHolder and onCreateViewHolder

---
+++ app/src/main/java/pl/tinks/budgetbuddy/payments/PaymentListAdapter.kt
@@ -0,0 +1,30 @@
+package pl.tinks.budgetbuddy.payments
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import pl.tinks.budgetbuddy.databinding.ItemPaymentBinding
+
+class PaymentListAdapter : RecyclerView.Adapter<PaymentListAdapter.PaymentListViewHolder>() {
+
+    class PaymentListViewHolder(private val binding: ItemPaymentBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaymentListViewHolder {
+        val binding = ItemPaymentBinding
+            .inflate(
+                LayoutInflater
+                    .from(parent.context), parent, false
+            )
+        return PaymentListViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PaymentListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
