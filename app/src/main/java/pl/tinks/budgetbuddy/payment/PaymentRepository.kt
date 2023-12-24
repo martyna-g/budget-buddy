2edbd946b08f8642e5fc8bd16b0a692347103243
Martyna Gorna <mart.gorna@gmail.com>
2023-12-24 12:39:58 +0000

    Add PaymentRepository and PaymentRepositoryImpl

---
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/PaymentRepository.kt
@@ -0,0 +1,17 @@
+package pl.tinks.budgetbuddy.payment
+
+import kotlinx.coroutines.flow.Flow
+import pl.tinks.budgetbuddy.Result
+import java.util.UUID
+
+interface PaymentRepository {
+
+    fun getAllPayments(): Flow<Result<List<Payment>>>
+
+    suspend fun getPaymentById(paymentId: UUID): Payment
+
+    suspend fun addPayment(payment: Payment)
+
+    suspend fun deletePayment(payment: Payment)
+
+}
---
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/PaymentRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package pl.tinks.budgetbuddy.payment
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import pl.tinks.budgetbuddy.Result
+import java.util.UUID
+
+class PaymentRepositoryImpl(private val paymentDao: PaymentDao) : PaymentRepository {
+
+    override fun getAllPayments(): Flow<Result<List<Payment>>> {
+        return paymentDao.getAllPayments().map { list ->
+            Result.Success(list) as Result<List<Payment>>
+        }.catch { e ->
+            emit(Result.Error(e))
+        }
+    }
+
+    override suspend fun getPaymentById(paymentId: UUID) = paymentDao.getPaymentById(paymentId)
+
+    override suspend fun addPayment(payment: Payment) = paymentDao.addPayment(payment)
+
+    override suspend fun deletePayment(payment: Payment) = paymentDao.deletePayment(payment)
+
+}
