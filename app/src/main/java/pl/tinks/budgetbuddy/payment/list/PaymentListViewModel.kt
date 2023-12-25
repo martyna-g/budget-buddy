d066ea570cdc729dbc359daea8c6106db055519d
Martyna Gorna <mart.gorna@gmail.com>
2023-12-25 11:19:25 +0000

    Add uiState property for observing payment data changes

--- app/src/main/java/pl/tinks/budgetbuddy/payment/list/PaymentListViewModel.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/list/PaymentListViewModel.kt
@@ -1,9 +1,27 @@
 package pl.tinks.budgetbuddy.payment.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import pl.tinks.budgetbuddy.Result
+import pl.tinks.budgetbuddy.payment.Payment
 import pl.tinks.budgetbuddy.payment.PaymentRepository
 
 class PaymentListViewModel(private val paymentRepository: PaymentRepository) : ViewModel() {
+
+    private var _uiState: StateFlow<PaymentUiState<List<Payment>>> =
+        paymentRepository.getAllPayments().map { result ->
+            when (result) {
+                is Result.Success -> PaymentUiState.Success(result.data)
+                is Result.Error -> PaymentUiState.Error(result.e)
+            }
+        }.stateIn(viewModelScope, SharingStarted.Lazily, PaymentUiState.Loading)
+
+    val uiState: Flow<PaymentUiState<List<Payment>>> = _uiState
 }
 
 sealed class PaymentUiState<out T> {
