bcb51ca62958b08f190730213d90edde18b57e8d
Martyna Gorna <mart.gorna@gmail.com>
2023-12-27 19:44:02 +0000

    Add navigation functionality from HomeFragment to PaymentListFragment

--- app/src/main/java/pl/tinks/budgetbuddy/home/HomeFragment.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/home/HomeFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import pl.tinks.budgetbuddy.R
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import pl.tinks.budgetbuddy.databinding.FragmentHomeBinding
+
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var upcomingPaymentsButton: Button
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        upcomingPaymentsButton = binding.buttonUpcomingPayments
+        upcomingPaymentsButton.setOnClickListener { navigateToPaymentList() }
+
+        return binding.root
     }
 
+    private fun navigateToPaymentList() {
+        val action = HomeFragmentDirections.actionHomeFragmentToPaymentListFragment()
+        findNavController().navigate(action)
+    }
 }
