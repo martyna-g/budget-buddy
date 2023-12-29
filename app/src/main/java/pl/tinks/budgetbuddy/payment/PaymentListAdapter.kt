f46041215525875985f9b373653aeb9817ca29ee
Martyna Gorna <mart.gorna@gmail.com>
2023-12-29 20:19:55 +0000

    Refactor PaymentListAdapter to extend ListAdapter and implement necessary members

--- app/src/main/java/pl/tinks/budgetbuddy/payment/PaymentListAdapter.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/PaymentListAdapter.kt
@@ -2,29 +2,49 @@ package pl.tinks.budgetbuddy.payment
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import pl.tinks.budgetbuddy.databinding.ItemPaymentBinding
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
-class PaymentListAdapter : RecyclerView.Adapter<PaymentListAdapter.PaymentListViewHolder>() {
+class PaymentListAdapter :
+    ListAdapter<Payment, PaymentListAdapter.PaymentListViewHolder>(PaymentDiffCallback()) {
 
     class PaymentListViewHolder(private val binding: ItemPaymentBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(payment: Payment) {
+            val date = ZonedDateTime.of(payment.date, ZoneId.of("UTC"))
+            with(binding) {
+                textPaymentTitle.text = payment.title
+                textPaymentAmount.text = payment.amount.toString()
+                textPaymentDateDay.text = date.dayOfMonth.toString()
+                textPaymentDateMonth.text = date.month.toString().substring(0..2)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaymentListViewHolder {
-        val binding = ItemPaymentBinding
-            .inflate(
-                LayoutInflater
-                    .from(parent.context), parent, false
-            )
+        val binding = ItemPaymentBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
         return PaymentListViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: PaymentListViewHolder, position: Int) {
+        holder.bind(getItem(position))
     }
 
-    override fun onBindViewHolder(holder: PaymentListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    class PaymentDiffCallback : DiffUtil.ItemCallback<Payment>() {
+        override fun areItemsTheSame(oldItem: Payment, newItem: Payment): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Payment, newItem: Payment): Boolean {
+            return oldItem == newItem
+        }
     }
+
 }
