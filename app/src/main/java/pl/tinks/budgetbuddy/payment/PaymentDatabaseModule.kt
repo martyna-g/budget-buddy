d5ef82d7bcd54bb93e6d7c8231d4ee18f6bbe962
Martyna Gorna <mart.gorna@gmail.com>
2024-01-04 16:45:17 +0000

    Create PaymentDatabase Hilt Module

---
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/PaymentDatabaseModule.kt
@@ -0,0 +1,24 @@
+package pl.tinks.budgetbuddy.payment
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object PaymentDatabaseModule {
+
+    @Singleton
+    @Provides
+    fun providePaymentDatabase(@ApplicationContext context: Context): PaymentDatabase {
+        return Room.databaseBuilder(
+            context, PaymentDatabase::class.java, "payment_database"
+        ).build()
+    }
+}
+
