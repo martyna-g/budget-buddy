cc994d04294739e654ab082658c3b2b337d44fd4
Martyna Gorna <mart.gorna@gmail.com>
2024-01-08 08:32:27 +0000

    Refactor PaymentViewModel for improved data loading and retry mechanism

--- app/src/main/java/pl/tinks/budgetbuddy/payment/list/PaymentListViewModel.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/list/PaymentListViewModel.kt
@@ -3,11 +3,11 @@ package pl.tinks.budgetbuddy.payment.list
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import pl.tinks.budgetbuddy.Result
 import pl.tinks.budgetbuddy.payment.Payment
 import pl.tinks.budgetbuddy.payment.PaymentRepository
@@ -17,15 +17,34 @@ import javax.inject.Inject
 class PaymentListViewModel @Inject constructor(private val paymentRepository: PaymentRepository) :
     ViewModel() {
 
-    private var _uiState: StateFlow<PaymentUiState> =
-        paymentRepository.getAllPayments().map { result ->
-            when (result) {
-                is Result.Success -> PaymentUiState.Success(result.data)
-                is Result.Error -> PaymentUiState.Error(result.e)
-            }
-        }.stateIn(viewModelScope, SharingStarted.Lazily, PaymentUiState.Loading)
+    private lateinit var job: Job
+    private var _uiState: MutableStateFlow<PaymentUiState> =
+        MutableStateFlow(PaymentUiState.Loading)
+    val uiState: StateFlow<PaymentUiState> = _uiState
 
-    val uiState: Flow<PaymentUiState> = _uiState
+    init {
+        loadPayments()
+    }
+
+    private fun loadPayments() {
+        job = viewModelScope.launch {
+            _uiState.value = PaymentUiState.Loading
+            paymentRepository.getAllPayments().map { result ->
+                when (result) {
+                    is Result.Success -> PaymentUiState.Success(result.data)
+                    is Result.Error -> PaymentUiState.Error(result.e)
+                }
+            }.collect {
+                _uiState.value = it
+            }
+        }
+    }
+
+    fun reloadPayments() {
+        job.cancel()
+        loadPayments()
+    }
+
 }
 
 sealed class PaymentUiState {
