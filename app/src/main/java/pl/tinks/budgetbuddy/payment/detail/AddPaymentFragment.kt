940ec756a1abd5f3046bfe11cad6ef19dae8c7cb
Martyna Gorna <mart.gorna@gmail.com>
2024-01-16 12:08:08 +0000

    Replace AlertDialog with a standard Dialog

--- app/src/main/java/pl/tinks/budgetbuddy/payment/detail/AddPaymentFragment.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/detail/AddPaymentFragment.kt
@@ -1,28 +1,71 @@
 package pl.tinks.budgetbuddy.payment.detail
 
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
 import pl.tinks.budgetbuddy.R
 import pl.tinks.budgetbuddy.databinding.FragmentAddPaymentBinding
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class AddPaymentFragment : DialogFragment() {
 
     private lateinit var binding: FragmentAddPaymentBinding
+    private lateinit var paymentTitleEditText: EditText
+    private lateinit var paymentAmountEditText: EditText
+    private lateinit var paymentDateEditText: EditText
+    private lateinit var paymentFrequencyTextView: AutoCompleteTextView
+    private lateinit var datePicker: MaterialDatePicker<Long>
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialog)
+    }
 
-        val builder = AlertDialog.Builder(requireActivity(), R.style.BudgetBuddy_FullScreenDialog)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
 
-        binding = FragmentAddPaymentBinding.inflate(layoutInflater)
+        binding = FragmentAddPaymentBinding.inflate(inflater, container, false)
 
-        builder.setView(binding.root)
+        val constraintsBuilder = CalendarConstraints.Builder()
+            .setValidator(DateValidatorPointForward.now())
 
-        return builder.create()
+        paymentTitleEditText = binding.textInputEditTextPaymentTitle
+        paymentAmountEditText = binding.textInputEditTextPaymentAmount
+        paymentDateEditText = binding.textInputEditTextPaymentDate
+        paymentFrequencyTextView = binding.autocompleteTextviewPaymentFrequency
+        datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select date")
+            .setCalendarConstraints(constraintsBuilder.build())
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+            .build()
+
+        datePicker.addOnPositiveButtonClickListener { dateInMillis ->
+            paymentDateEditText.setText(
+                LocalDateTime.ofInstant(Instant.ofEpochMilli(dateInMillis), ZoneId.of("UTC"))
+                    .format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+            )
+        }
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
     }
 
 }
