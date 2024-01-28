fe7d5aad8764782a2592c9cf54e574f1023be1d6
Martyna Gorna <mart.gorna@gmail.com>
2024-01-28 15:16:37 +0000

    Add addPayment function

--- app/src/main/java/pl/tinks/budgetbuddy/payment/detail/AddPaymentFragment.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/detail/AddPaymentFragment.kt
@@ -13,14 +13,18 @@ import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.DateValidatorPointForward
 import com.google.android.material.datepicker.MaterialDatePicker
 import dagger.hilt.android.AndroidEntryPoint
+import org.joda.money.CurrencyUnit
+import org.joda.money.Money
 import pl.tinks.budgetbuddy.DecimalDigitsInputFilter
 import pl.tinks.budgetbuddy.R
 import pl.tinks.budgetbuddy.databinding.FragmentAddPaymentBinding
+import pl.tinks.budgetbuddy.payment.Payment
 import pl.tinks.budgetbuddy.payment.PaymentFrequency
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.util.UUID
 
 private const val DATE_PICKER_ADD_PAYMENT = "DATE_PICKER_ADD_PAYMENT"
 
@@ -36,6 +40,7 @@ class AddPaymentFragment : DialogFragment() {
     private lateinit var datePicker: MaterialDatePicker<Long>
     private lateinit var selectedDate: LocalDateTime
     private lateinit var paymentFrequencies: Map<PaymentFrequency, String>
+    private val currencyGbp: CurrencyUnit = CurrencyUnit.GBP
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -103,6 +108,29 @@ class AddPaymentFragment : DialogFragment() {
         dialog?.window?.setLayout(
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.MATCH_PARENT
+        )
+    }
+
+    private fun addPayment() {
+
+        val uuid = UUID.randomUUID()
+
+        val title = paymentTitleEditText.text.toString()
+        val amount = paymentAmountEditText.text.toString()
+        val frequency = paymentFrequencyTextView.text.toString()
+
+        val amountMoney: Money = Money.of(currencyGbp, amount.toDoubleOrNull() ?: 0.00)
+        val paymentFrequency: PaymentFrequency =
+            paymentFrequencies.entries.find { it.value == frequency }?.key!!
+
+        viewModel.addPayment(
+            Payment(
+                uuid,
+                title,
+                amountMoney,
+                selectedDate,
+                paymentFrequency
+            )
         )
     }
 
