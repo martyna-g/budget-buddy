30ff986a91f8c2dcf0cfafdb3dccb984845b83d4
Martyna Gorna <mart.gorna@gmail.com>
2024-02-19 19:42:50 +0000

    Add helper class for next schedule date calculation

---
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/list/PaymentDateCalculator.kt
@@ -0,0 +1,28 @@
+package pl.tinks.budgetbuddy.payment.list
+
+import pl.tinks.budgetbuddy.payment.PaymentFrequency
+import java.time.LocalDate
+import javax.inject.Inject
+
+class PaymentDateCalculator @Inject constructor() {
+
+    fun calculateNextScheduleDate(
+        paymentDate: LocalDate,
+        paymentFrequency: PaymentFrequency
+    ): LocalDate? {
+
+        val nextScheduleDate = when (paymentFrequency) {
+            PaymentFrequency.SINGLE_PAYMENT -> paymentDate
+            PaymentFrequency.DAILY -> paymentDate.plusDays(1)
+            PaymentFrequency.WEEKLY -> paymentDate.plusDays(7)
+            PaymentFrequency.MONTHLY -> paymentDate.plusMonths(1)
+            PaymentFrequency.QUARTERLY -> paymentDate.plusMonths(4)
+            PaymentFrequency.BIANNUALLY -> paymentDate.plusMonths(6)
+            PaymentFrequency.ANNUALLY -> paymentDate.plusMonths(12)
+        }
+
+        return nextScheduleDate
+
+    }
+
+}
