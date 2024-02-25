089cd389b3cb8d8b4690f87d4462b2caff7c76be
Martyna Gorna <mart.gorna@gmail.com>
2024-02-25 18:55:20 +0000

    Add PaymentScheduler Hilt Module

---
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/PaymentSchedulerModule.kt
@@ -0,0 +1,19 @@
+package pl.tinks.budgetbuddy.payment
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import pl.tinks.budgetbuddy.payment.list.PaymentScheduler
+import pl.tinks.budgetbuddy.payment.list.PaymentSchedulerImpl
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class PaymentSchedulerModule {
+
+    @Singleton
+    @Binds
+    abstract fun bindPaymentScheduler(paymentScheduler: PaymentSchedulerImpl) : PaymentScheduler
+
+}
