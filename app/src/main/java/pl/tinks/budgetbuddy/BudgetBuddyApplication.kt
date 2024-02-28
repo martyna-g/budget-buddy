48542f3e1ee5927cecf6c4a12540392c933307e3
Martyna Gorna <mart.gorna@gmail.com>
2024-02-28 09:01:58 +0000

    Update Application to use HiltWorkerFactory for WorkManager configuration

--- app/src/main/java/pl/tinks/budgetbuddy/BudgetBuddyApplication.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/BudgetBuddyApplication.kt
@@ -1,7 +1,20 @@
 package pl.tinks.budgetbuddy
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class BudgetBuddyApplication : Application() {}
+class BudgetBuddyApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
+}
