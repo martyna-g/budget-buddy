b1271d94ab08d8a2822e9d281c8c66788ad8d9e2
Martyna Gorna <mart.gorna@gmail.com>
2024-03-21 16:17:01 +0000

    Add ShoppingItem entity

---
+++ app/src/main/java/pl/tinks/budgetbuddy/shopping/ShoppingItem.kt
@@ -0,0 +1,13 @@
+package pl.tinks.budgetbuddy.shopping
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "shopping_items")
+data class ShoppingItem(
+    @PrimaryKey val id: UUID,
+    @ColumnInfo(name = "item_name") val itemName: String,
+    @ColumnInfo(name = "is_collected") val isCollected: Boolean = false,
+)
