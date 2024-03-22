fd457c20cda09ca81f48e0972fd91b5506321f7b
Martyna Gorna <mart.gorna@gmail.com>
2024-03-22 19:14:36 +0000

    Add ShoppingItem entity and ShoppingDao to BudgetBuddyDatabase

--- app/src/main/java/pl/tinks/budgetbuddy/BudgetBuddyDatabase.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/BudgetBuddyDatabase.kt
@@ -8,11 +8,13 @@ import pl.tinks.budgetbuddy.payment.NextPaymentRequestDao
 import pl.tinks.budgetbuddy.payment.Payment
 import pl.tinks.budgetbuddy.payment.PaymentDao
 import pl.tinks.budgetbuddy.payment.PaymentRoomConverters
+import pl.tinks.budgetbuddy.shopping.ShoppingDao
+import pl.tinks.budgetbuddy.shopping.ShoppingItem
 
-@Database(entities = [Payment::class, NextPaymentRequest::class], version = 1)
+@Database(entities = [Payment::class, NextPaymentRequest::class, ShoppingItem::class], version = 1)
 @TypeConverters(PaymentRoomConverters::class)
 abstract class BudgetBuddyDatabase : RoomDatabase() {
     abstract fun getPaymentDao(): PaymentDao
     abstract fun getNextPaymentRequestDao(): NextPaymentRequestDao
-
+    abstract fun getShoppingDao(): ShoppingDao
 }
