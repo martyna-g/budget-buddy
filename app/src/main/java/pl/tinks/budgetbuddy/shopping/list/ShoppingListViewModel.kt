f9c4894cdca230ea61f56cbd389df2c994ab405a
Martyna Gorna <mart.gorna@gmail.com>
2024-03-23 17:09:33 +0000

    Add ShoppingListUiState sealed class

--- app/src/main/java/pl/tinks/budgetbuddy/shopping/list/ShoppingListViewModel.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/shopping/list/ShoppingListViewModel.kt
@@ -2,6 +2,7 @@ package pl.tinks.budgetbuddy.shopping.list
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import pl.tinks.budgetbuddy.shopping.ShoppingItem
 import pl.tinks.budgetbuddy.shopping.ShoppingRepository
 import javax.inject.Inject
 
@@ -12,3 +13,8 @@ class ShoppingListViewModel @Inject constructor(
 
 }
 
+sealed class ShoppingListUiState {
+    data object Loading : ShoppingListUiState()
+    data class Success(val data: List<ShoppingItem>) : ShoppingListUiState()
+    data class Error(val e: Throwable) : ShoppingListUiState()
+}
