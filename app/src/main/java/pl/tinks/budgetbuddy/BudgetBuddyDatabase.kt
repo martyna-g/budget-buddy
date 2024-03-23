5a3e4e2a5ffd2b7127a71a933b57514b3636b393
Martyna Gorna <mart.gorna@gmail.com>
2024-03-23 16:07:25 +0000

    Update TypeConverters reference

--- app/src/main/java/pl/tinks/budgetbuddy/BudgetBuddyDatabase.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/BudgetBuddyDatabase.kt
@@ -7,12 +7,11 @@ import pl.tinks.budgetbuddy.payment.NextPaymentRequest
 import pl.tinks.budgetbuddy.payment.NextPaymentRequestDao
 import pl.tinks.budgetbuddy.payment.Payment
 import pl.tinks.budgetbuddy.payment.PaymentDao
-import pl.tinks.budgetbuddy.payment.PaymentRoomConverters
 import pl.tinks.budgetbuddy.shopping.ShoppingDao
 import pl.tinks.budgetbuddy.shopping.ShoppingItem
 
 @Database(entities = [Payment::class, NextPaymentRequest::class, ShoppingItem::class], version = 1)
-@TypeConverters(PaymentRoomConverters::class)
+@TypeConverters(RoomTypeConverter::class)
 abstract class BudgetBuddyDatabase : RoomDatabase() {
     abstract fun getPaymentDao(): PaymentDao
     abstract fun getNextPaymentRequestDao(): NextPaymentRequestDao
