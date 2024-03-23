1576aac46c55c794f024d07a3a1edf538339a58f
Martyna Gorna <mart.gorna@gmail.com>
2024-03-23 11:23:05 +0000

    Add implementation for ShoppingRepository

---
+++ app/src/main/java/pl/tinks/budgetbuddy/shopping/ShoppingRepositoryImpl.kt
@@ -0,0 +1,45 @@
+package pl.tinks.budgetbuddy.shopping
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import pl.tinks.budgetbuddy.Result
+import java.util.UUID
+import javax.inject.Inject
+
+class ShoppingRepositoryImpl @Inject constructor(
+    private val shoppingDao: ShoppingDao
+) : ShoppingRepository {
+
+    override fun getAllShoppingItems(): Flow<Result<List<ShoppingItem>>> {
+        return shoppingDao.getAllShoppingItems().map { shoppingList ->
+            Result.Success(shoppingList) as Result<List<ShoppingItem>>
+        }.catch { e ->
+            emit(Result.Error(e))
+        }
+    }
+
+    override fun getShoppingItemsByCollectedStatus(collected: Boolean): Flow<Result<List<ShoppingItem>>> {
+        return shoppingDao.getItemsByCollectedStatus(collected).map { shoppingList ->
+            Result.Success(shoppingList) as Result<List<ShoppingItem>>
+        }.catch { e ->
+            emit(Result.Error(e))
+        }
+    }
+
+    override suspend fun getShoppingItemById(id: UUID): ShoppingItem {
+        return shoppingDao.getShoppingItemById(id)
+    }
+
+    override suspend fun addShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingDao.addShoppingItem(shoppingItem)
+    }
+
+    override suspend fun updateShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingDao.updateShoppingItem(shoppingItem)
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingDao.deleteShoppingItem(shoppingItem)
+    }
+}
