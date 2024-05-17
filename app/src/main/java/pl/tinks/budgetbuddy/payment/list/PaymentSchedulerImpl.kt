2355e664a2bd1958b30b0b56fcd7aff65d420b80
Martyna Gorna <mart.gorna@gmail.com>
2024-05-17 14:43:28 +0100

    Add logic to enqueue notification work in PaymentSchedulerImpl

--- app/src/main/java/pl/tinks/budgetbuddy/payment/list/PaymentSchedulerImpl.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/list/PaymentSchedulerImpl.kt
@@ -5,11 +5,12 @@ import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.workDataOf
 import pl.tinks.budgetbuddy.payment.PaymentFrequency
-import pl.tinks.budgetbuddy.payment.PaymentRepository
 import pl.tinks.budgetbuddy.payment.NextPaymentRequestDao
 import pl.tinks.budgetbuddy.payment.NextPaymentRequest
 import pl.tinks.budgetbuddy.payment.Payment
+import pl.tinks.budgetbuddy.payment.PaymentNotificationWorker
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.ZoneOffset
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
@@ -25,18 +26,38 @@ class PaymentSchedulerImpl @Inject constructor(
 
             val nextPaymentDateEpochSeconds = payment.date.toEpochSecond(ZoneOffset.UTC)
             val nowEpochSeconds = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)
+            val notificationTimeEpochSeconds =
+                payment.date.minusDays(1).with(LocalTime.NOON).toEpochSecond(ZoneOffset.UTC)
 
-            val delay = nextPaymentDateEpochSeconds - nowEpochSeconds
+            val paymentDelay = nextPaymentDateEpochSeconds - nowEpochSeconds
+            val notificationDelay = notificationTimeEpochSeconds - nowEpochSeconds
+
             val id = payment.id.toString()
 
-            val workRequest = OneTimeWorkRequestBuilder<RecurringPaymentWorker>()
-                .setInitialDelay(delay, TimeUnit.SECONDS)
+            val paymentWorkRequest = OneTimeWorkRequestBuilder<RecurringPaymentWorker>()
+                .setInitialDelay(paymentDelay, TimeUnit.SECONDS)
                 .setInputData(workDataOf("paymentId" to id))
                 .build()
 
+
+            val notificationWorkRequest = if (notificationDelay > 0) {
+                OneTimeWorkRequestBuilder<PaymentNotificationWorker>()
+                    .setInitialDelay(notificationDelay, TimeUnit.SECONDS)
+                    .setInputData(
+                        workDataOf(
+                            PaymentNotificationWorker.PAYMENT_TITLE_KEY to payment.title,
+                            PaymentNotificationWorker.PAYMENT_ID_KEY to id
+                        )
+                    )
+                    .build()
+            } else {
+                null
+            }
+
             nextPaymentRequestDao.addNextPaymentRequest(
                 NextPaymentRequest(
-                    workRequest.id,
+                    paymentWorkRequest.id,
+                    notificationWorkRequest?.id,
                     payment.id
                 )
             )
@@ -44,13 +65,23 @@ class PaymentSchedulerImpl @Inject constructor(
             workManager.enqueueUniqueWork(
                 "Payment_$id",
                 ExistingWorkPolicy.KEEP,
-                workRequest
+                paymentWorkRequest
             )
+
+            if (notificationWorkRequest != null) {
+                workManager.enqueueUniqueWork(
+                    "Notification_$id",
+                    ExistingWorkPolicy.KEEP,
+                    notificationWorkRequest
+                )
+            }
         }
+
     }
 
     override suspend fun cancelUpcomingPayment(payment: Payment) {
-        val nextPaymentRequest: NextPaymentRequest? = nextPaymentRequestDao.getNextPaymentRequestByPaymentId(payment.id)
+        val nextPaymentRequest: NextPaymentRequest? =
+            nextPaymentRequestDao.getNextPaymentRequestByPaymentId(payment.id)
         if (nextPaymentRequest != null) {
             workManager.cancelWorkById(nextPaymentRequest.requestId)
             nextPaymentRequestDao.deleteNextPaymentRequest(nextPaymentRequest)
