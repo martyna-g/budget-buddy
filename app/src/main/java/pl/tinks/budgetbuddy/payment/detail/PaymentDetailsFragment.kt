5fa3594c6d57a9834f81e318f3b0ffb89cc989a2
Martyna Gorna <mart.gorna@gmail.com>
2024-05-19 18:59:54 +0100

    Add notification permission request logic to PaymentDetailsFragment

--- app/src/main/java/pl/tinks/budgetbuddy/payment/detail/PaymentDetailsFragment.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/detail/PaymentDetailsFragment.kt
@@ -1,5 +1,8 @@
 package pl.tinks.budgetbuddy.payment.detail
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -10,7 +13,10 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -19,6 +25,7 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.DateValidatorPointForward
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.materialswitch.MaterialSwitch
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import org.joda.money.CurrencyUnit
@@ -51,6 +58,7 @@ class PaymentDetailsFragment : DialogFragment() {
     private lateinit var datePicker: MaterialDatePicker<Long>
     private lateinit var selectedDate: LocalDateTime
     private lateinit var paymentFrequencies: Array<out String>
+    private lateinit var notificationSwitch: MaterialSwitch
     private lateinit var toolbar: MaterialToolbar
     private val currencyGbp: CurrencyUnit = CurrencyUnit.GBP
     private val args: PaymentDetailsFragmentArgs by navArgs()
@@ -59,10 +67,21 @@ class PaymentDetailsFragment : DialogFragment() {
     private lateinit var payment: Payment
     private var hasUnsavedChanges = false
     private var isInitializing = true
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setStyle(STYLE_NORMAL, R.style.FullScreenDialog)
+
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+                if (!isGranted) {
+                    Toast.makeText(
+                        context, R.string.payment_notification_permission_denied, Toast.LENGTH_SHORT
+                    ).show()
+                    notificationSwitch.isChecked = false
+                }
+            }
     }
 
     override fun onCreateView(
@@ -81,6 +100,7 @@ class PaymentDetailsFragment : DialogFragment() {
         paymentAmountEditText = binding.textInputEditTextPaymentAmount
         paymentDateEditText = binding.textInputEditTextPaymentDate
         paymentFrequencyTextView = binding.autocompleteTextviewPaymentFrequency
+        notificationSwitch = binding.switchPaymentNotification
         toolbar = binding.toolbarAddPayment
         paymentFrequencies = resources.getStringArray(R.array.payment_frequencies)
 
@@ -168,6 +188,10 @@ class PaymentDetailsFragment : DialogFragment() {
             addTextChangedListener(DecimalDigitsInputFilter(this))
         }
 
+        notificationSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) checkAndRequestNotificationPermission()
+        }
+
         paymentTitleEditText.addTextChangedListener(validateFieldsTextWatcher)
         paymentAmountEditText.addTextChangedListener(validateFieldsTextWatcher)
         paymentDateEditText.addTextChangedListener(validateFieldsTextWatcher)
@@ -197,18 +221,54 @@ class PaymentDetailsFragment : DialogFragment() {
 
     private fun createPaymentFromUserInput(id: UUID): Payment {
         val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+
         val title = paymentTitleEditText.text.toString()
         val amount = paymentAmountEditText.text.toString()
         val localDate = LocalDate.parse(paymentDateEditText.text, formatter)
         val frequency = paymentFrequencyTextView.text.toString()
 
         val amountMoney: Money = Money.of(currencyGbp, amount.toDoubleOrNull() ?: 0.00)
-        val paymentFrequency: PaymentFrequency = mapStringToPaymentFrequency(frequency)
         val date = LocalDateTime.of(localDate, LocalTime.MIDNIGHT)
+        val paymentFrequency: PaymentFrequency = mapStringToPaymentFrequency(frequency)
+        val notificationEnabled = notificationSwitch.isChecked
 
         return Payment(
-            id, title, amountMoney, date, paymentFrequency
+            id,
+            title,
+            amountMoney,
+            date,
+            paymentFrequency,
+            notificationEnabled = notificationEnabled
         )
+    }
+
+    private fun checkAndRequestNotificationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireActivity(), Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                showPermissionRationale()
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+        }
+    }
+
+
+    private fun showPermissionRationale() {
+        AlertDialog.Builder(requireActivity())
+            .setTitle(R.string.payment_notification_permission_title)
+            .setMessage(R.string.payment_notification_permission_message)
+            .setPositiveButton(R.string.dialog_ok) { _, _ ->
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }.setNegativeButton(R.string.dialog_cancel) { _, _ ->
+                notificationSwitch.isChecked = false
+            }.show()
     }
 
     private fun populateFields(payment: Payment) {
@@ -220,20 +280,6 @@ class PaymentDetailsFragment : DialogFragment() {
             payment.date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
         )
         isInitializing = false
-    }
-
-    private fun areFieldsValid(): Boolean {
-
-        val valid: Boolean
-
-        val title = paymentTitleEditText.text.toString()
-        val amount = paymentAmountEditText.text.toString()
-        val date = paymentDateEditText.text.toString()
-        val frequency = paymentFrequencyTextView.text.toString()
-
-        valid = !(title.isBlank() || amount.isBlank() || date.isBlank() || frequency.isBlank())
-
-        return valid
     }
 
     private fun mapStringToPaymentFrequency(frequencyInput: String): PaymentFrequency {
@@ -284,15 +330,26 @@ class PaymentDetailsFragment : DialogFragment() {
     }
 
     private fun showDiscardChangesConfirmationDialog() {
-        AlertDialog.Builder(requireContext())
-            .setMessage(R.string.discard_changes_message)
+        AlertDialog.Builder(requireContext()).setMessage(R.string.discard_changes_message)
             .setPositiveButton(R.string.yes) { _, _ ->
                 dismiss()
-            }
-            .setNegativeButton(R.string.no) { dialog, _ ->
+            }.setNegativeButton(R.string.no) { dialog, _ ->
                 dialog.dismiss()
-            }
-            .show()
+            }.show()
+    }
+
+    private fun areFieldsValid(): Boolean {
+
+        val valid: Boolean
+
+        val title = paymentTitleEditText.text.toString()
+        val amount = paymentAmountEditText.text.toString()
+        val date = paymentDateEditText.text.toString()
+        val frequency = paymentFrequencyTextView.text.toString()
+
+        valid = !(title.isBlank() || amount.isBlank() || date.isBlank() || frequency.isBlank())
+
+        return valid
     }
 
     private val validateFieldsTextWatcher = object : TextWatcher {
