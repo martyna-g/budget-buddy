53783b8f5c9a02f97867dd745f53b04a58ad5677
Martyna Gorna <mart.gorna@gmail.com>
2024-06-04 14:31:59 +0100

    Update BankHolidayHeaderAdapter to use item_bank_holiday_header for item views

--- app/src/main/java/pl/tinks/budgetbuddy/bank_holiday/BankHolidayListHeaderAdapter.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/bank_holiday/BankHolidayListHeaderAdapter.kt
@@ -3,20 +3,22 @@ package pl.tinks.budgetbuddy.bank_holiday
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import pl.tinks.budgetbuddy.databinding.ItemHeaderBinding
+import pl.tinks.budgetbuddy.databinding.ItemBankHolidayHeaderBinding
 
 class BankHolidayListHeaderAdapter(private val headerTitle: String) :
     RecyclerView.Adapter<BankHolidayListHeaderAdapter.HeaderViewHolder>() {
 
-    class HeaderViewHolder(val binding: ItemHeaderBinding) : RecyclerView.ViewHolder(binding.root)
+    class HeaderViewHolder(val binding: ItemBankHolidayHeaderBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HeaderViewHolder {
-        val binding = ItemHeaderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemBankHolidayHeaderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return HeaderViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: HeaderViewHolder, position: Int) {
-        holder.binding.textHeader.text = headerTitle
+        holder.binding.layoutBankHolidayHeader.textHeader.text = headerTitle
     }
 
     override fun getItemCount(): Int = 1
