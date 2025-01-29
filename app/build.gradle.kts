8171d52fbcb2735e60bf6da7c8e356130829f00a
Martyna Gorna <mart.gorna@gmail.com>
2025-01-29 20:49:12 +0000

    Add Mockito dependencies for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,9 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.room:room-testing:$roomVersion")
+    testImplementation("org.mockito:mockito-core:5.15.2")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.navigation:navigation-testing:$navVersion")
