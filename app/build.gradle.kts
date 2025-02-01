4e995b069ebab53c56a87f23b49ca398fabb0a02
Martyna Gorna <mart.gorna@gmail.com>
2025-02-01 14:41:59 +0000

    Add MainDispatcherRule to replace Dispatchers.Main in tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,9 +96,11 @@ dependencies {
     testImplementation("androidx.room:room-testing:$roomVersion")
     testImplementation("org.mockito:mockito-core:5.15.2")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.navigation:navigation-testing:$navVersion")
     androidTestImplementation("androidx.work:work-testing:$workVersion")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
