5347a8a45c66d3fb87b036dab29fcdf7fffb20f4
Martyna Gorna <mart.gorna@gmail.com>
2025-02-15 18:33:39 +0000

    Refactor PaymentDaoTest with Hilt and improve structure

--- app/src/androidTest/java/pl/tinks/budgetbuddy/PaymentDaoTest.kt
+++ app/src/androidTest/java/pl/tinks/budgetbuddy/PaymentDaoTest.kt
@@ -1,80 +1,125 @@
 package pl.tinks.budgetbuddy
 
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.SmallTest
-import kotlinx.coroutines.runBlocking
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.nullValue
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.containsInAnyOrder
 import org.joda.money.CurrencyUnit
 import org.joda.money.Money
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import pl.tinks.budgetbuddy.payment.Payment
 import pl.tinks.budgetbuddy.payment.PaymentDao
 import pl.tinks.budgetbuddy.payment.PaymentFrequency
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 import java.util.UUID
+import javax.inject.Inject
 
-@RunWith(AndroidJUnit4::class)
-@SmallTest
+@HiltAndroidTest
 class PaymentDaoTest {
 
-    private lateinit var database: BudgetBuddyDatabase
-    private lateinit var paymentDao: PaymentDao
-    private val paymentId = UUID.randomUUID()
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var database: BudgetBuddyDatabase
+
+    @Inject
+    lateinit var paymentDao: PaymentDao
+
     private lateinit var payment: Payment
+    private lateinit var payments: List<Payment>
+    private val paymentId = UUID.randomUUID()
 
     @Before
-    fun setupDatabase() {
-        database = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(), BudgetBuddyDatabase::class.java
-        ).build()
-
-        paymentDao = database.getPaymentDao()
+    fun setUp() {
+        hiltRule.inject()
 
         payment = Payment(
             paymentId,
-            "Rent",
-            Money.of(CurrencyUnit.GBP, 111.11),
+            "Test Payment",
+            Money.of(CurrencyUnit.GBP, 10.00),
             LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS),
             PaymentFrequency.DAILY
         )
 
-        runBlocking {
-            paymentDao.addPayment(payment)
-        }
+        payments = listOf(
+            payment,
+            payment.copy(id = UUID.randomUUID(), title = "Test Payment 2"),
+            payment.copy(id = UUID.randomUUID(), title = "Test Payment 3")
+        )
     }
 
     @After
-    fun closeDatabase() {
+    fun tearDown() {
         database.close()
     }
 
     @Test
-    fun getById_returnsCorrectPayment() = runBlocking {
+    fun getAllPayments_returnsAllPayments() = runTest {
+        payments.forEach { paymentDao.addPayment(it) }
+
+        val result = paymentDao.getAllPayments().first()
+        val expectedResult = payments.toTypedArray()
+
+        assertThat(result, containsInAnyOrder(*expectedResult))
+    }
+
+    @Test
+    fun getPaymentById_returnsCorrectPayment() = runTest {
+        paymentDao.addPayment(payment)
+
         val paymentById = paymentDao.getPaymentById(paymentId)
         assertThat(paymentById, `is`(payment))
     }
 
     @Test
-    fun updatePayment_updatesPaymentInDb() = runBlocking {
-        val updatedPayment = payment.copy(title = "Updated Rent")
+    fun updatePayment_updatesPaymentInDatabase() = runTest {
+        paymentDao.addPayment(payment)
+
+        val updatedPayment = payment.copy(title = "Updated Title")
+
         paymentDao.updatePayment(updatedPayment)
 
-        val retrievedPayment = paymentDao.getPaymentById(paymentId)
-        assertThat(retrievedPayment.title, `is`("Updated Rent"))
+        val result = paymentDao.getPaymentById(paymentId)
+
+        assertThat(result?.title, `is`("Updated Title"))
     }
 
     @Test
-    fun deletePayment_removesPaymentFromDb() = runBlocking {
+    fun deletePayment_removesPaymentFromDatabase() = runTest {
+        paymentDao.addPayment(payment)
+
+        val resultBeforeDelete = paymentDao.getPaymentById(paymentId)
+
+        assertThat(resultBeforeDelete, `is`(payment))
+
         paymentDao.deletePayment(payment)
-        val retrievedPayment = paymentDao.getPaymentById(paymentId)
-        assertThat(retrievedPayment, `is`(nullValue()))
+
+        val resultAfterDelete = paymentDao.getPaymentById(paymentId)
+
+        assertThat(resultAfterDelete, `is`(nullValue()))
+    }
+
+    @Test
+    fun deletePayments_removesAllSpecifiedPaymentsFromDatabase() = runTest {
+        payments.forEach { paymentDao.addPayment(it) }
+
+        val resultBeforeDelete = paymentDao.getAllPayments().first()
+
+        assertThat(resultBeforeDelete, containsInAnyOrder(*payments.toTypedArray()))
+
+        paymentDao.deletePayments(payments)
+
+        val resultAfterDelete = paymentDao.getAllPayments().first()
+
+        assertThat(resultAfterDelete, `is`(emptyList()))
     }
 }
