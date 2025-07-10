7329e1a961485664f59599b3ea183b73fe83af20
Martyna Gorna <mart.gorna@gmail.com>
2025-07-10 13:02:07 +0100

    Add ItemHeader Composable

---
+++ app/src/main/java/pl/tinks/budgetbuddy/ItemHeader.kt
@@ -0,0 +1,29 @@
+package pl.tinks.budgetbuddy
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ItemHeader(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.titleMedium,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp)
+    )
+}
+
+@Preview(apiLevel = 33, showBackground = true)
+@Preview(apiLevel = 33, uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun ItemHeaderPreview() {
+    ItemHeader(text = "Preview Text")
+}
