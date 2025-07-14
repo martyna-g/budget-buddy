2ce73373224cb25d4275158da2b93eaefff44c23
Martyna Gorna <mart.gorna@gmail.com>
2025-07-14 21:31:10 +0100

    Refactor PaymentFrequency to use string resources for labels

--- app/src/main/java/pl/tinks/budgetbuddy/payment/list/PaymentFrequency.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/payment/list/PaymentFrequency.kt
@@ -1,11 +1,19 @@
 package pl.tinks.budgetbuddy.payment.list
 
-enum class PaymentFrequency {
-    SINGLE_PAYMENT,
-    DAILY,
-    WEEKLY,
-    MONTHLY,
-    QUARTERLY,
-    BIANNUALLY,
-    ANNUALLY
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import pl.tinks.budgetbuddy.R
+
+enum class PaymentFrequency(@StringRes val labelRes: Int) {
+    SINGLE_PAYMENT(R.string.single_payment),
+    DAILY(R.string.daily),
+    WEEKLY(R.string.weekly),
+    MONTHLY(R.string.monthly),
+    QUARTERLY(R.string.quarterly),
+    BIANNUALLY(R.string.biannually),
+    ANNUALLY(R.string.annually)
 }
+
+@Composable
+fun PaymentFrequency.getLabel(): String = stringResource(labelRes)
