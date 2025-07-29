0fd62f7d1f3d49fca7795a6e10a3ae8d97028071
Martyna Gorna <mart.gorna@gmail.com>
2025-07-29 20:09:53 +0100

    Migrate MainActivity to Compose

--- app/src/main/java/pl/tinks/budgetbuddy/MainActivity.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/MainActivity.kt
@@ -1,38 +1,26 @@
 package pl.tinks.budgetbuddy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         var keepSplash = true
         val delayTime = 800L
 
         installSplashScreen().setKeepOnScreenCondition { keepSplash }
         Handler(Looper.getMainLooper()).postDelayed({ keepSplash = false }, delayTime)
 
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
-
-        setContentView(R.layout.activity_main)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
-        val navController = navHostFragment.navController
-
-        findViewById<BottomNavigationView>(R.id.bottom_navigation)
-            .setupWithNavController(navController)
+        setContent {
+            MainScreen()
+        }
     }
 }
