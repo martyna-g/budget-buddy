df56b86576625a62f7777a83d19bf614fdfca045
Martyna Gorna <mart.gorna@gmail.com>
2025-08-20 16:23:34 +0100

    Enable edge-to-edge system bar with automatic icon contrast

--- app/src/main/java/pl/tinks/budgetbuddy/MainActivity.kt
+++ app/src/main/java/pl/tinks/budgetbuddy/MainActivity.kt
@@ -1,10 +1,13 @@
 package pl.tinks.budgetbuddy
 
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import pl.tinks.budgetbuddy.ui.theme.BudgetBuddyTheme
@@ -19,6 +22,11 @@ class MainActivity : ComponentActivity() {
         Handler(Looper.getMainLooper()).postDelayed({ keepSplash = false }, delayTime)
 
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
 
         setContent {
             BudgetBuddyTheme {
